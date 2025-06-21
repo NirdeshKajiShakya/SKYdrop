2d23183a154ffa8b7703814617d8e5aa2ee30831
NirdeshKajiShakya <nirkajishakya23@gmail.com>
2025-06-22 00:47:30 +0545

    post made visiable till 9

--- src/skydrop/userDAO/PostDao.java
+++ src/skydrop/userDAO/PostDao.java
@@ -4,13 +4,17 @@
  */
 package skydrop.userDAO;
 
+import java.awt.Image;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 import skydrop.database.DbConnection;
 
 /**
@@ -40,7 +44,7 @@ public class PostDao {
     
     
     public String getProductNameDB(int product_id) {
-        String query = "SELECT product_name FROM users where product_id = ?";
+        String query = "SELECT product_name FROM products where product_id = ?";
 
         try (Connection conn = DbConnection.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(query)) {
@@ -50,6 +54,75 @@ public class PostDao {
             
             if (rs.next()){
                 return rs.getString("product_name");
+            }else{
+                return null;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public Image displayImageFromDatabase(int id) {
+        try (Connection conn = DbConnection.getConnection()) {
+            String query = "SELECT picture FROM products WHERE product_id = ?";
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setInt(1, id);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                byte[] imgBytes = rs.getBytes("picture");
+
+                if (imgBytes != null && imgBytes.length > 0) {
+                    ImageIcon icon = new ImageIcon(imgBytes);
+                    Image img = icon.getImage().getScaledInstance(161, 149, Image.SCALE_SMOOTH);
+                    return img;
+                } 
+
+            }
+            return null;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            
+            return null;
+        }
+    }
+    
+    public int getProductPriceDB(int product_id) {
+        String query = "SELECT price FROM products where product_id = ?";
+
+        try (Connection conn = DbConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+               
+            pstmt.setInt(1, product_id);
+            ResultSet rs = pstmt.executeQuery();
+            
+            if (rs.next()){
+                return rs.getInt("price");
+            }else{
+                return 0;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+    
+    public String getProductCategoryDB(int product_id) {
+        String query = "SELECT category FROM products where product_id = ?";
+
+        try (Connection conn = DbConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+               
+            pstmt.setInt(1, product_id);
+            ResultSet rs = pstmt.executeQuery();
+            
+            if (rs.next()){
+                return rs.getString("category");
             }else{
                 return null;
             }
