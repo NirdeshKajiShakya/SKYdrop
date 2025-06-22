02a558b4ace0c3d7042e684fdeaaa11a4a39cea6
Pranabudhathoki <pranabudhathoki@gmail.com>
2025-06-23 01:48:27 +0545

    Forget password development

---
+++ src/skydrop/utils/EmailSender.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package skydrop.utils;
+
+/**
+ *
+ * @author Suresh
+ */
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class EmailSender {
+
+    public static boolean sendVerificationCode(String to, String code) {
+        String from = "your-email@gmail.com";
+        String host = "smtp.gmail.com";
+        String password = "your-app-password"; // use Gmail App Password
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(props,
+            new Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(from, password);
+                }
+            });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+            message.setSubject("Password Reset Verification Code");
+            message.setText("Your code is: " + code);
+
+            Transport.send(message);
+            return true;
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
+
